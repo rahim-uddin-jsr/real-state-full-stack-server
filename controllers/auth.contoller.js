ecc44914c48a54a690c9de2e8770335dbeb754b2
dev-rahim <rahimuddin5915@gmail.com>
2023-10-09 19:57:11 +0600

    creat signin api with jwt auth

--- controllers/auth.contoller.js
+++ controllers/auth.contoller.js
@@ -1,5 +1,7 @@
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/errorHandaler.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPass = bcryptjs.hashSync(password, 10);
@@ -7,6 +9,23 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("user created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const { password: pass, ...rest } = validUser._doc;
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credential!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
