878e73db3f3a030f50c6cf7b7a6b78ba06b843b7
dev-rahim <rahimuddin5915@gmail.com>
2023-10-09 14:51:56 +0600

    connected to mongodb using mongoose

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
-require("dotenv").config();
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
-
 app.use(cors());
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.db_user_name}:${process.env.db_pass}@cluster0.vt0qgrn.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("ok I am well!!");
