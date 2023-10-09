18718e33a8eb2b723b03d5682971b6883b95a7ed
dev-rahim <rahimuddin5915@gmail.com>
2023-10-09 14:36:29 +0600

    initail api project created for realstate project

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("ok I am well!!");
+});
+
+app.listen(port, () => {
+  console.log("server is running on port =>", port);
+});
